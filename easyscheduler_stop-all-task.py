acc6c45b4da2201cd0767385f4a959058fb557e5
striver619 <51977051+striver619@users.noreply.github.com>
2022-09-16 17:55:06 +0800

    easyscheduler_stop-all-task_plus

--- easyscheduler_stop-all-task.py
+++ easyscheduler_stop-all-task.py
@@ -3,23 +3,21 @@
 
 import requests
 
-pageUrl = "http://ip1:port/escheduler/projects/dw/instance/list-paging?searchVal=&pageSize=1000&pageNo=1" \
+pageUrl = "http://192.168.1.6:12345/escheduler/projects/dw/instance/list-paging?searchVal=&pageSize=1000&pageNo=1" \
           "&host=&stateType=RUNNING_EXEUTION&startDate=&endDate= "
 
-# 获取 Cookie:
-# EasyScheduler 工作流实例 页面，浏览器 F12
-# Network --> Fetch/XHR --> F5 刷新页面 --> Name --> list-paging?searchVal=&page
-# Headers --> Request Headers --> Cookie
+resp = requests.post(url='http://192.168.1.6:12345/escheduler/login',
+                     headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                     data={'userName': 'zhangsan', 'userPassword': '123456'})
 
-sessionId = 'eexxx76-5xx4-4xx4-ad96-xxx8'
+sessionId = resp.headers.get('Set-Cookie').split("=")[1]
 
-resp = requests.get(url=pageUrl, headers={'Cookie': 'sessionId=' + sessionId + ';language=zh_CN',
-                                          'X-Forwarded-for': 'ip2'})
+resp2 = requests.get(url=pageUrl, headers={'Cookie': 'sessionId=' + sessionId + ';language=zh_CN'})
 
-data = resp.json()['data']['totalList']
+data = resp2.json()['data']['totalList']
+
 for e in data:
-    resp = requests.post(url='http://ip1:port/escheduler/projects/dw/executors/execute',
+    resp2 = requests.post(url='http://192.168.1.6:12345/escheduler/projects/dw/executors/execute',
                          headers={'Content-Type': 'application/x-www-form-urlencoded',
-                                  'Cookie': 'sessionId=' + sessionId + ';language=zh_CN',
-                                  'X-Forwarded-for': 'ip2'},
+                                  'Cookie': 'sessionId=' + sessionId + ';language=zh_CN'},
                          data = {'processInstanceId': e['id'], 'executeType': 'STOP'})
